78aca3d7fee32bd6b299f4abe57fddf6a748812b
ydbweb <ydbweb.com@gmail.com>
2026-01-25 16:44:41 +0100

    to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActorComponent } from './actor/actor.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { filmactorService } from './datasource.service';
 import { FormactorComponent } from './formactor/formactor.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ActorComponent,
-    FormactorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [filmactorService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ActorComponent,
+        FormactorComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [filmactorService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
